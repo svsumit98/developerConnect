945302fe9eb508a6c7a2f35daa6c756bc9bda7aa
Sumit Verma <svsumit9810@gmail.com>
2025-01-16 01:39:56 +0530

    Change in auth.js middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,29 +2,29 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 const userAuth = async (req, res, next) => {
-  try{
+  try {
     // Read the token from the req cookies
     const { token } = req.cookies;
-    
+
     // Validate the token
-    if(!token){
-      throw new Error("Invalid Token!!");
+    if (!token) {
+      return res.status(401).send("Please Login!");
     }
 
     // Find the user
-    const decodedObj = await jwt.verify(token, "DEV@Connect@123");  // returns an object with _id , exp, iat
+    const decodedObj = await jwt.verify(token, "DEV@Connect@123"); // returns an object with _id , exp, iat
     const { _id } = decodedObj;
     const user = await User.findById(_id);
-    if(!user){
+    if (!user) {
       throw new Error("User not found");
     }
     req.user = user;
     next();
-  } catch (err){
+  } catch (err) {
     res.status(400).send("ERROR: " + err.message);
   }
 };
 
 module.exports = {
-  userAuth,
+  userAuth
 };
