05faed7b5b33ad51ae934edfbb07ebe0d43c2575
Sumit Verma <svsumit9810@gmail.com>
2025-01-23 19:04:01 +0530

    Bug Fixed: Ignored

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -1,40 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectionRequestSchema = new mongoose.Schema({
-
-  fromUserId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User", // referance to the user collection.
-    required: true,
-  }, 
-  toUserId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  }, 
-  status: {
-    type: String,
-    required:true,
-    enum: {
-      values: ["ignore", "interested", "accepted", "rejected"],
-      message: `{VALUE} is incorrect status type`
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // referance to the user collection.
+      required: true
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["ignored", "interested", "accepted", "rejected"],
+        message: `{VALUE} is incorrect status type`
+      }
     }
-  } 
-}, 
-  { timestamps: true,}
+  },
+  { timestamps: true }
 );
 
-connectionRequestSchema.index({fromUserId: 1, toUserId: 1});
+connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
 
 connectionRequestSchema.pre("save", function (next) {
   const connectionRequest = this;
   // Check if the fromUserId is same as toUserId
-  if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
     throw new Error("Cannot send connection request to yourself!");
   }
   next();
 });
 
-const ConnectionRequestModel = new mongoose.model("ConnectionRequest", connectionRequestSchema);
+const ConnectionRequestModel = new mongoose.model(
+  "ConnectionRequest",
+  connectionRequestSchema
+);
 
 module.exports = ConnectionRequestModel;
